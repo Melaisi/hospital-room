2ab8e0db64853ea5bf88eba8ec4cb322e872cbde
Mohammed Melaisi <mohammed.melaisi@gmail.com>
2021-07-31 18:39:47 -0400

    Update Task to keep track of how many items are at the end location and trigger validation method to update the score.

--- Assets/Hospital Game/Scripts/Task.cs
+++ Assets/Hospital Game/Scripts/Task.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Task : MonoBehaviour
 {
+    public List<GameObject> itemsPrefab = new List<GameObject>();
     public List<GameObject> items = new List<GameObject>();
     public List<Transform> startingLocation = new List<Transform>();
     public List<Transform> endLocation = new List<Transform>();
 
-    
+    int itemsInEndLocation = 0;
 
     private void Start()
     {
@@ -19,17 +20,20 @@ public class Task : MonoBehaviour
     {
         // instantiate items randomly
         // shuffle items
-        //items.Shuffle();
+        // items.Shuffle();
         // shuffle starting position instead 
         startingLocation.Shuffle();
         // instantiate items to starting location  
         // todo: make sure avaliable starting locations 
-        for (int i=0; i<items.Count; i++)
+        for (int i=0; i< itemsPrefab.Count; i++)
         {
-            var item = Instantiate(items[i], startingLocation[i].position,items[i].transform.rotation);
-            //assign end location 
+            var item = Instantiate(itemsPrefab[i], startingLocation[i].position, itemsPrefab[i].transform.rotation);
+            // assign end location 
             item.GetComponent<Item>().endLocation = endLocation[i].gameObject;
-
+            // assign this task to each item 
+            item.GetComponent<Item>().task = this;
+            // add item ref to items list 
+            items.Add(item.gameObject);
         }
        
     }
@@ -45,6 +49,7 @@ public class Task : MonoBehaviour
         {
             if (item.GetComponent<Item>().itemInPlace)
             {
+
                 score++;
             }
         }
@@ -52,4 +57,23 @@ public class Task : MonoBehaviour
 
     }
     
+    // to be called be called by ItemMovment everytime an item inter or exist [should be static somewhere for easy access] 
+    public void increaseItemsInEndLocation()
+    {
+        itemsInEndLocation++;
+
+        // check if all items in their end location then calculate the score and end the game 
+        if (items.Count == itemsInEndLocation)
+        {
+            validate();
+        }
+    }
+
+    public void decreaseItemsInEndLocation()
+    {
+        if(itemsInEndLocation > 0)
+        {
+            itemsInEndLocation--; 
+        }
+    }
 }
