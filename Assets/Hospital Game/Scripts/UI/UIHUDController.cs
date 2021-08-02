467de090037bb5d1c37fafd5d1ba7622de7215d0
Mohammed Melaisi <mohammed.melaisi@gmail.com>
2021-08-02 13:49:39 -0400

    HUD controller to change game state and a prefab

--- Assets/Hospital Game/Scripts/UI/UIHUDController.cs
+++ Assets/Hospital Game/Scripts/UI/UIHUDController.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIHUDController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Button objectiveBtn;
+    public Button exitBtn;
+    public Button pauseBtn;
+
+    private void Start()
     {
-        
+        objectiveBtn.onClick.AddListener(showObjective);
+        exitBtn.onClick.AddListener(exitGame);
+        pauseBtn.onClick.AddListener(pauseGame);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void pauseGame()
     {
-        
+        GameController.instance.PauseGame();
+    }
+
+    private void exitGame()
+    {
+        GameController.instance.EndGame();
+    }
+
+    private void showObjective()
+    {
+        GameController.instance.ShowObjective();
     }
 }
