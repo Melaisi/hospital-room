bb0218623887aa95d87484a28d5b2937bea04a8b
Mohammed Melaisi <mohammed.melaisi@gmail.com>
2021-08-02 15:02:35 -0400

    Restart level after completion

--- Assets/Hospital Game/Scripts/UI/UILevelSummaryController.cs
+++ Assets/Hospital Game/Scripts/UI/UILevelSummaryController.cs
@@ -1,8 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UILevelSummaryController : MonoBehaviour
 {
-    
+    public Button restart;
+
+    private void Start()
+    {
+        restart.onClick.AddListener(restartLevel);
+    }
+
+    private void restartLevel()
+    {
+        GameController.instance.RestartGame();
+    }
 }
