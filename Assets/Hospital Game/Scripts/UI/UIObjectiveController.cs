da756e976bd89b02268fe3c0703ed93daab943aa
Mohammed Melaisi <mohammed.melaisi@gmail.com>
2021-08-01 22:42:24 -0400

    Objective canvas button can change game state to playing.

--- Assets/Hospital Game/Scripts/UI/UIObjectiveController.cs
+++ Assets/Hospital Game/Scripts/UI/UIObjectiveController.cs
@@ -1,8 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIObjectiveController : MonoBehaviour
 {
-   
+    
+    public Button play;
+
+    private void Start()
+    {
+        play.onClick.AddListener(startPlaying);
+    }
+
+    private void startPlaying()
+    {
+        GameController.instance.StartPlaying();
+    }
 }
