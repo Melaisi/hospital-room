aae7e777b19b9b2eb256cbcd36c83e921a41b97c
Mohammed Melaisi <mohammed.melaisi@gmail.com>
2021-08-02 22:40:48 -0400

    Update score when showing the level summary

--- Assets/Hospital Game/Scripts/UI/UILevelSummaryController.cs
+++ Assets/Hospital Game/Scripts/UI/UILevelSummaryController.cs
@@ -3,18 +3,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class UILevelSummaryController : MonoBehaviour
 {
     public Button restart;
+    public TMP_Text levelSummary;
+    public TMP_Text taskSummary;
 
     private void Start()
     {
         restart.onClick.AddListener(restartLevel);
+
+       
+        updateTaskDetails();
+        updateLevelDetails();
+
+    }
+   
+
+    private void updateLevelDetails()
+    {
+        levelSummary.SetText($"Your Score is {GameController.instance.getLevelScore()}");
+    }
+
+    private void updateTaskDetails()
+    {
+        taskSummary.SetText($"{GameController.instance.getTaskScore()} items out of 4 has been correctly placed in their location. ");
     }
 
     private void restartLevel()
     {
         GameController.instance.RestartGame();
     }
+
 }
